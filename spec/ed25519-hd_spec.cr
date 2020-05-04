42f72cefe1233f88a206e0837b27e080ab46b50f
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2020-05-04 19:24:17 +0100

    updates

--- spec/ed25519-hd_spec.cr
+++ spec/ed25519-hd_spec.cr
@@ -20,18 +20,19 @@ describe ED25519::HD do
 
   it "should get a child private key" do
     master_keys = KeyRing.get_master_key_from_seed(vector_1.seed)
-
-    
-    # seed = "000102030405060708090a0b0c0d0e0f"
-    # private_key = "2b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e7"
-    # chain_code = "90046a93de5380a72b5e45010748567d5ea02bbf6522f979e05c0d8d8ca9fffb"
-    # expected = "002b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e700000000"
-
-
-
     child_keys = KeyRing.ckd_priv(master_keys, 0)
     child_keys.private_key.should eq("563a4b69932c3cf7c39721775f2a9f279ba146696ea768d38aceefdcd32dbc87")
-    # child_keys.chain_code.should eq("17b756364c345478348fad89b6f13f783268468cac8c7d6cc3a2654dce9c3c79")
+    child_keys.chain_code.should eq("17b756364c345478348fad89b6f13f783268468cac8c7d6cc3a2654dce9c3c79")
+  end
+
+  it "should get a public key from the private key (with zero byte)" do
+    master_keys = KeyRing.get_master_key_from_seed(vector_1.seed)
+    KeyRing.get_public_key(master_keys.private_key).should eq("00a4b2856bfec510abab89753fac1ac0e1112364e7d250545963f135f2a33188ed")
+  end
+
+  it "should get a public key from the private key (no with zero byte)" do
+    master_keys = KeyRing.get_master_key_from_seed(vector_1.seed)
+    KeyRing.get_public_key(master_keys.private_key, false).should eq("a4b2856bfec510abab89753fac1ac0e1112364e7d250545963f135f2a33188ed")
   end
 
   # describe "Vector 2" do
--- src/ed25519-hd.cr
+++ src/ed25519-hd.cr
@@ -3,18 +3,6 @@ require "monocypher"
 module ED25519::HD
   ED25519_CURVE = "ed25519 seed"
 
-  struct Crypto
-    def self.get_keys(seed : String, key : String) : Keys
-      digest = uninitialized UInt8[64]
-      message = seed.hexbytes
-      LibMonocypher.hmac_sha512(digest, key, key.bytesize, message, message.bytesize)
-      private_key = digest.to_slice[0, 32].hexstring
-      chain_code = digest.to_slice[32, 32].hexstring
-
-      Keys.new(private_key, chain_code)
-    end
-  end
-
   HARDENED_OFFSET = 0x80000000
 
   struct Keys
@@ -25,8 +13,18 @@ module ED25519::HD
   end
 
   struct KeyRing
+    def self.get_keys(seed : Slice(UInt8), key : Slice(UInt8)) : Keys
+      digest = uninitialized UInt8[64]
+      message = seed
+      LibMonocypher.hmac_sha512(digest, key, key.bytesize, message, message.bytesize)
+      private_key = digest.to_slice[0, 32].hexstring
+      chain_code = digest.to_slice[32, 32].hexstring
+
+      Keys.new(private_key, chain_code)
+    end
+
     def self.get_master_key_from_seed(seed : String) : Keys
-      Crypto.get_keys(seed, ED25519_CURVE)
+      get_keys(seed.hexbytes, ED25519_CURVE.to_slice)
     end
 
     def self.ckd_priv(keys : Keys, index : Int32) : Keys
@@ -34,20 +32,20 @@ module ED25519::HD
       seed[1..32].copy_from(keys.private_key.hexbytes)
       seed[0] = 0x00
       seed[33] = index.to_u8
-
-      digest = uninitialized UInt8[64]
-      key = keys.chain_code.hexbytes
-      message = seed
-      LibMonocypher.hmac_sha512(digest, key, key.bytesize, message, message.bytesize)
-      private_key = digest.to_slice[0, 32].hexstring
-      chain_code = digest.to_slice[32, 32].hexstring
-      Keys.new(private_key, chain_code)
-
-      # Crypto.get_keys(seed.hexstring, keys.chain_code)
+      get_keys(seed, keys.chain_code.hexbytes)
     end
 
-    def self.get_public_key(private_key : String) : String
-      ""
+    def self.get_public_key(private_key : String, with_zero_byte : Bool = true) : String
+      secret_key = Crypto::SecretKey.new(private_key)
+      bytes = Crypto::Ed25519PublicSigningKey.new(secret: secret_key).to_slice
+      if with_zero_byte
+        key = Slice(UInt8).new(33)
+        key[0] = 0x00
+        key[1..32].copy_from(bytes)
+        key.hexstring
+      else
+        bytes.hexstring
+      end
     end
 
     def self.is_valid_path(path : String) : Bool
