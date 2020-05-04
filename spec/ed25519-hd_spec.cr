24ffd4e6e652a88b648411e4f605fe65ba3f47d8
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2020-05-04 18:49:23 +0100

    updates

--- spec/ed25519-hd_spec.cr
+++ spec/ed25519-hd_spec.cr
@@ -1,35 +1,52 @@
 require "./spec_helper"
 
 describe ED25519::HD do
+
   describe "Vector 1" do
-    it "should have valid keys for vector 1 seed hex" do
+    it "should have valid keys for vector 1 seed hex (chain m)" do
       keys = KeyRing.get_master_key_from_seed(vector_1.seed)
-      keys.key.should eq("2b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e7")
+      keys.private_key.should eq("2b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e7")
       keys.chain_code.should eq("90046a93de5380a72b5e45010748567d5ea02bbf6522f979e05c0d8d8ca9fffb")
     end
-    it "should be valid for vector path" do
-      vector_1.vectors.each do |v|
-        keys = KeyRing.derive_path(v.path, vector_1.seed)
-        keys.key.should eq(v.key)
-        keys.chain_code.should eq(v.chain_code)
-        KeyRing.get_public_key(v.key).should eq(v.public_key)
-      end
-    end
+    # it "should be valid for vector path" do
+    #   vector_1.vectors.each do |v|
+    #     keys = KeyRing.derive_path(v.path, vector_1.seed)
+    #     keys.private_key.should eq(v.key)
+    #     keys.chain_code.should eq(v.chain_code)
+    #     # KeyRing.get_public_key(v.key).should eq(v.public_key)
+    #   end
+    # end
   end
 
-  describe "Vector 2" do
-    it "should have valid keys for vector 2 seed hex" do
-      keys = KeyRing.get_master_key_from_seed(vector_2.seed)
-      keys.key.should eq("171cb88b1b3c1db25add599712e36245d75bc65a1a5c9e18d76f9f2b1eab4012")
-      keys.chain_code.should eq("ef70a74db9c3a5af931b5fe73ed8e1a53464133654fd55e7a66f8570b8e33c3b")
-    end
-    it "should be valid for vector path" do
-      vector_2.vectors.each do |v|
-        keys = KeyRing.derive_path(v.path, vector_2.seed)
-        keys.key.should eq(v.key)
-        keys.chain_code.should eq(v.chain_code)
-        KeyRing.get_public_key(v.key).should eq(v.public_key)
-      end
-    end
+  it "should get a child private key" do
+    master_keys = KeyRing.get_master_key_from_seed(vector_1.seed)
+
+    
+    # seed = "000102030405060708090a0b0c0d0e0f"
+    # private_key = "2b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e7"
+    # chain_code = "90046a93de5380a72b5e45010748567d5ea02bbf6522f979e05c0d8d8ca9fffb"
+    # expected = "002b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e700000000"
+
+
+
+    child_keys = KeyRing.ckd_priv(master_keys, 0)
+    child_keys.private_key.should eq("563a4b69932c3cf7c39721775f2a9f279ba146696ea768d38aceefdcd32dbc87")
+    # child_keys.chain_code.should eq("17b756364c345478348fad89b6f13f783268468cac8c7d6cc3a2654dce9c3c79")
   end
+
+  # describe "Vector 2" do
+  #   it "should have valid keys for vector 2 seed hex" do
+  #     keys = KeyRing.get_master_key_from_seed(vector_2.seed)
+  #     keys.key.should eq("171cb88b1b3c1db25add599712e36245d75bc65a1a5c9e18d76f9f2b1eab4012")
+  #     keys.chain_code.should eq("ef70a74db9c3a5af931b5fe73ed8e1a53464133654fd55e7a66f8570b8e33c3b")
+  #   end
+  #   it "should be valid for vector path" do
+  #     vector_2.vectors.each do |v|
+  #       keys = KeyRing.derive_path(v.path, vector_2.seed)
+  #       keys.key.should eq(v.key)
+  #       keys.chain_code.should eq(v.chain_code)
+  #       KeyRing.get_public_key(v.key).should eq(v.public_key)
+  #     end
+  #   end
+  # end
 end
--- src/ed25519-hd.cr
+++ src/ed25519-hd.cr
@@ -1,20 +1,42 @@
+require "monocypher"
+
 module ED25519::HD
+  ED25519_CURVE = "ed25519 seed"
+
+  struct Crypto
+    def self.get_keys(seed : String, key : String) : Keys
+      digest = uninitialized UInt8[64]
+      message = seed.hexbytes
+      LibMonocypher.hmac_sha512(digest, key, key.bytesize, message, message.bytesize)
+      private_key = digest.to_slice[0, 32].hexstring
+      chain_code = digest.to_slice[32, 32].hexstring
+
+      Keys.new(private_key, chain_code)
+    end
+  end
+
   HARDENED_OFFSET = 0x80000000
 
   struct Keys
-    getter key : String
+    getter private_key : String
     getter chain_code : String
 
-    def initialize(@key : String, @chain_code : String); end
+    def initialize(@private_key : String, @chain_code : String); end
   end
 
   struct KeyRing
     def self.get_master_key_from_seed(seed : String) : Keys
-      Keys.new("", "")
+      Crypto.get_keys(seed, ED25519_CURVE)
     end
 
     def self.ckd_priv(keys : Keys, index : Int32) : Keys
-      Keys.new("", "")
+      seed = Slice(UInt8).new(37)
+      seed[1..32].copy_from(keys.private_key.hexbytes)
+      seed[0] = 0x00
+      seed[33] = index.to_u8
+
+      puts seed.hexstring
+      Crypto.get_keys(seed.hexstring, keys.chain_code)
     end
 
     def self.get_public_key(private_key : String) : String
